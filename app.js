2f94dbd32dd47e361d42d44edfdeaf66bd2f3c18
ivandobrica <dobrica.ivan@hotmail.com>
2021-12-01 16:28:22 +0100

    check/delete buttons, filter todos

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const input = document.querySelector(".form__input");
 const addBtn = document.querySelector(".form__button");
 const todoList = document.querySelector(".todo__list");
+const todoFilter = document.querySelector(".select__filter");
 
 //event listeners
 addBtn.addEventListener("click", createTodo);
+todoList.addEventListener("click", checkDelete);
+todoFilter.addEventListener("click", filterTodo);
 
 //create todo items
 function createTodo(e) {
@@ -35,4 +38,42 @@ function createTodo(e) {
   //reset value and append to ul
   input.value = "";
   todoList.appendChild(todoDiv);
+}
+
+function checkDelete(e) {
+  const item = e.target;
+  if (item.classList.contains("complete__button")) {
+    const todo = item.parentElement;
+    todo.classList.toggle("completed");
+  }
+
+  if (item.classList.contains("delete__button")) {
+    const todo = item.parentElement;
+    todo.remove();
+  }
+}
+
+function filterTodo(e) {
+  const todos = todoList.childNodes;
+  todos.forEach(function (todo) {
+    switch (e.target.value) {
+      case "all":
+        todo.style.display = "flex";
+        break;
+      case "completed":
+        if (todo.classList.contains("completed")) {
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+        break;
+      case "uncompleted":
+        if (!todo.classList.contains("completed")) {
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+        break;
+    }
+  });
 }
